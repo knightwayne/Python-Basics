3d5ea79b24660dc4639f6d08b90b8c5a8de02e91
knightwayne <arnav7ishaan@gmail.com>
2019-10-05 08:44:44 +0530

    factorial

---
+++ factorial.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+print(factorial(5))
+print("Give Input")
+n=input()
+int(n)
+x=0
+while int(x)<int(n):
+    print(x"space"factorial(int(x)))
+    x=x+1
+    
